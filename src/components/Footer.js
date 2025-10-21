5c38816c8e6d184e4d92252c5c5438ab0d4d6345
Snehasis4321 <96995340+Snehasis4321@users.noreply.github.com>
2025-10-21 09:36:11 +0000

    fixes other

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer style={{ backgroundColor: '#333', color: 'white', padding: '1rem', textAlign: 'center' }}>
-      <p>&copy; 2023 Capybara Electronics. All rights reserved.</p>
+      <p>&copy; 2025 Capybara Electronics. All rights reserved.</p>
     </footer>
   );
 }
